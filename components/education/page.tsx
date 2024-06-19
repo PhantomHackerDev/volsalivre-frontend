96fd719f7d41dc855569b568f356fc6e82c97cc4
stuartyang1130 <stuartyang1130@gmail.com>
2024-06-19 06:34:27 -0700

    fix: education page Filter type field add

--- components/education/page.tsx
+++ components/education/page.tsx
@@ -4,7 +4,7 @@ const Education: React.FC = () => {
   return (
     <div className="flex flex-col lg:px-40 px-10 gap-5 py-5">
       <div className="flex justify-center gap-5">
-        <Filters />
+        <Filters type={""} />
       </div>
     </div>
   );
